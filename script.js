8c63b0eba0bcc6bef2dcd73ae605d195ca84df67
Florian846 <florian.sass1311@gmail.com>
2024-10-02 07:42:56 +0200

    Implement Load Button

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function loadProfiles() {
             let xmlDoc = parser.parseFromString(data, 'text/xml');
             let profiles = xmlDoc.getElementsByTagName('profile');
             let dropdownContent = document.getElementById('dropdown-content');
+            let selectedProfile = document.getElementById('selected-profile');
             
             for (let i = 0; i < profiles.length; i++) {
                 let name = profiles[i].getElementsByTagName('name')[0].childNodes[0].nodeValue;
@@ -27,6 +28,20 @@ function loadProfiles() {
                 profileItem.appendChild(img);
                 profileItem.appendChild(text);
                 dropdownContent.appendChild(profileItem);
+
+                // Klick-Event für das Dropdown-Element
+                profileItem.addEventListener('click', function() {
+                    // Gewähltes Profil im Button anzeigen
+                    selectedProfile.innerHTML = '';
+                    let selectedImg = document.createElement('img');
+                    selectedImg.src = imgSrc;
+                    selectedImg.alt = name;
+                    let selectedText = document.createElement('span');
+                    selectedText.textContent = name;
+
+                    selectedProfile.appendChild(selectedImg);
+                    selectedProfile.appendChild(selectedText);
+                });
             }
         })
         .catch(error => console.log('Error loading XML:', error));
